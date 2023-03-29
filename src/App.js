9fddf2939dc0f5c257036c5da0ff3319a5b406ca
bahasaracoglu <bahasaracoglu@gmail.com>
2023-03-29 16:24:34 +0300

    Veriler eklendi. Basit sayfa oluşturuldu.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,47 @@
 import React from "react";
 import "./App.css";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [data, setData] = useState();
+  useEffect(() => {
+    axios
+      .get(
+        "https://api.nasa.gov/planetary/apod?api_key=UlsVoK8kcwqrZsjMU3GBTYLF0xxcbp7DpgafkE0n"
+      )
+      .then(function (response) {
+        setData(response.data);
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }, []);
+
   return (
     <div className="App">
+      <h1>
+        Astronomy Photo Of The Day
+        <span role="img" aria-label="go!">
+          🚀
+        </span>
+      </h1>
       <p>
-        NASA uygulamasını yapmak için README.md dosyasıdaki talimatları takip edin
-		İyi eğlenceler! <span role="img" aria-label='go!'>🚀</span>!
+        Explore the mysteries of the universe! Each day, a unique image or
+        photograph of distant galaxies, stunning nebulas, and other celestial
+        wonders is highlighted, complemented by a brief description written by
+        an experienced astronomer.
+      </p>
+
+      <p>{data.date}</p>
+      <img src={data.url}></img>
+      <p>{data.title}</p>
+      <p>
+        Image Credit & Copyright: <span>{data.copyright}</span>
+      </p>
+      <p>
+        Explanation: <span>{data.explanation}</span>
       </p>
     </div>
   );
